0367ace5473fef467b65246391ee0e70cb275b34
dianKoliva <dianpretty01@gmail.com>
2022-01-03 13:16:38 +0200

    new changes

--- km/src/components/employees/RegisterEmp.jsx
+++ km/src/components/employees/RegisterEmp.jsx
@@ -7,29 +7,30 @@ const RegisterEmp = () => {
 
 <div className=' text-sm bg-box py-10 px-10 cnt2  w-2/5 mt-52  '>
     <h1 className='text-center mb-4 text-def font-bold'>Register new KM employee</h1>
-  <form action="
-  ">
+  <form action=" 
+  " className='pl-14 mt-8'>
  <div>
     <p>Amazina yombi</p>
-    <input className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
+    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
     </div>
- <div className='mt-4'>
+ <div className='mt-2'>
     <p>Tel no</p>
-    <input className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
+    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
     </div>
- <div className='mt-4'>
+ <div className='mt-2'>
     <p>Umushahara</p>
-    <input className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
+    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
     </div>
 
  
-    <div>
-        <input type="checkbox" name="" id="" />
-        Agree to terms and conditions 
+    <div className='mt-2 flex'>
+        <input type="checkbox" className='mt-1' name="" id="" />
+        <p className='ml-1'>  Agree to terms and conditions </p>
+       
     </div>
 
-<div>
-    <button>Save</button>
+<div className='mt-4'>
+    <button className='bg-def text-white text-sm w-9/12 rounded-md py-1'>Save</button>
 </div>
 
 
