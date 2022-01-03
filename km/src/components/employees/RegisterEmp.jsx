b045497b1f52ee2316a8b4fd9b583b3330ba1e80
dianKoliva <dianpretty01@gmail.com>
2022-01-03 12:34:01 +0200

    new changes

---
+++ km/src/components/employees/RegisterEmp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from '../../layouts/Dashboard'
+
+const RegisterEmp = () => {
+    return (
+           <Dashboard>
+
+
+            </Dashboard>
+    
+    )
+}
+
+export default RegisterEmp
