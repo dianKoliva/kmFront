19b4768ebf671659b3c961cbcc33e11e476e3508
dianKoliva <dianpretty01@gmail.com>
2022-01-03 13:18:30 +0200

    new changes

--- km/src/components/employees/RegisterEmp.jsx
+++ km/src/components/employees/RegisterEmp.jsx
@@ -5,21 +5,21 @@ const RegisterEmp = () => {
     return (
            <Dashboard>
 
-<div className=' text-sm bg-box py-10 px-10 cnt2  w-2/5 mt-52  '>
+<div className=' text-sm bg-box py-10 px-10 cnt2  w-2/5 mt-24  '>
     <h1 className='text-center mb-4 text-def font-bold'>Register new KM employee</h1>
   <form action=" 
   " className='pl-14 mt-8'>
  <div>
     <p>Amazina yombi</p>
-    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
+    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-10/12 ' type="text" />
     </div>
  <div className='mt-2'>
     <p>Tel no</p>
-    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
+    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-10/12 ' type="text" />
     </div>
  <div className='mt-2'>
     <p>Umushahara</p>
-    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-9/12 ' type="text" />
+    <input spellCheck="false" className=' px-2 py-1   mt-1 focus:outline-none border rounded-md w-10/12 ' type="text" />
     </div>
 
  
@@ -30,7 +30,7 @@ const RegisterEmp = () => {
     </div>
 
 <div className='mt-4'>
-    <button className='bg-def text-white text-sm w-9/12 rounded-md py-1'>Save</button>
+    <button className='bg-def text-white text-sm w-10/12 rounded-md py-1'>Save</button>
 </div>
 
 
