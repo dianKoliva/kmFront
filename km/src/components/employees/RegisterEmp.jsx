772941c2740d4ffc3df9dc33450b9221fd721797
dianKoliva <dianpretty01@gmail.com>
2022-01-03 12:40:33 +0200

    new changes

--- km/src/components/employees/RegisterEmp.jsx
+++ km/src/components/employees/RegisterEmp.jsx
@@ -5,6 +5,32 @@ const RegisterEmp = () => {
     return (
            <Dashboard>
 
+<div>
+    <h1>Register new KM employee</h1>
+  <form action="
+  ">
+ <div>
+    <p>Amazina yombi</p>
+    <input type="text" />
+    </div>
+ <div>
+    <p>Tell no</p>
+    <input type="text" />
+    </div>
+ <div>
+    <p>Umushahara</p>
+    <input type="text" />
+    </div>
+
+    <div>
+        <input type="checkbox" name="" id="" />
+        Agree to terms and conditions 
+    </div>
+
+  </form>
+   
+  
+</div>
 
             </Dashboard>
     
