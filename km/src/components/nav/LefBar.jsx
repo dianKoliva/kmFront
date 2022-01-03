806c9b6e427198b40325d4355a7445be49966d9e
dianKoliva <dianpretty01@gmail.com>
2022-01-03 11:34:08 +0200

    new changes

--- km/src/components/nav/LefBar.jsx
+++ km/src/components/nav/LefBar.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const LefBar = () => {
     return (
         <div className=' bg-purple h-screen '>
-            hello
+            <h1>K</h1>
+            <p>oliva motors</p>
+
+        
         </div>
     )
 }
