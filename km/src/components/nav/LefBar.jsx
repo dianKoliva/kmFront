d492c58a6a7c5b1466080bb21738ca54ffafce48
dianKoliva <dianpretty01@gmail.com>
2022-01-03 12:00:45 +0200

    new changes

--- km/src/components/nav/LefBar.jsx
+++ km/src/components/nav/LefBar.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 const LefBar = () => {
     return (
-        <div className=' bg-purple h-screen '>
+        <div className=' bg-purple h-screen pl-6 '>
            
-<div> <h1>K</h1>
-            <p>oliva motors</p>
+<div className='flex'> <h1 className='font-bold text-4xl '>K</h1>
+            <p className='mt-4 text-sm'>oliva motors</p>
 
 </div>
+
+<div className='mt-20 '>
+    <ul>
+        <li>Home</li>
+        <li>Attendance</li>
+        <li>Register</li>
+       
+    </ul>
+</div>
+
 
 
         
