6a0854166379abd9217caed99ff4f098305ae364
dianKoliva <dianpretty01@gmail.com>
2022-01-05 12:26:15 +0200

    new changes

--- km/src/App.jsx
+++ km/src/App.jsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import "./styles/index.css"
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Attendance from './components/employees/Attendance'
 import ViewEmp from './components/employees/ViewEmp'
 import Signup from "./components/forms/Signup"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    {/* <ViewEmp></ViewEmp> */}
-    <Signup></Signup>
+
+    <Router>
+    <Switch>
+    <Route path="/km/" exact component={Login} />
+        <Route path="/km/signup" exact component={ Signup } />
+    </Switch>
+    </Router>
     </div>
   )
 }
--- km/src/main.jsx
+++ km/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Route, Link, BrowserRouter } from 'react-router-dom'   
+import Signup from './components/forms/Signup'
+import Login from "./components/forms/Login"
+
+
 
 ReactDOM.render(
   <React.StrictMode>
